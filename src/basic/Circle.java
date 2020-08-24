f89c3a307609473bc0e23c26d8df9ffbea471f05
santoshi215 <70130604+santoshi215@users.noreply.github.com>
2020-08-24 22:10:03 +0530

    commit

---
+++ src/basic/Circle.java
@@ -0,0 +1,26 @@
+package basic;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		
+		float pi = (float) 3.14;
+		int rad = 5;
+		
+		System.out.println("THE PI VALUE IS:"+pi);
+		
+		System.out.println("radios of circle is:"+rad);
+		
+		float area=(pi*rad*rad);
+		float perimeter=(2*pi*rad);
+		
+		System.out.println("Area of circle is:" +area);
+		System.out.println("Perimeter of circle is:" +perimeter);
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
