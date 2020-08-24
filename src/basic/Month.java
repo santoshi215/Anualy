d8e9058fca4d91195ae98da790bfd41f301033ab
santoshi215 <70130604+santoshi215@users.noreply.github.com>
2020-08-24 22:17:15 +0530

    mon

---
+++ src/basic/Month.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Month {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int k=2;
+          
+          System.out.println(k++ - ++k + k-- - k++ + ++k - k-- + --k + k + k-- - k + --k + k++);
+          
+          
+
+	}
+
+}
